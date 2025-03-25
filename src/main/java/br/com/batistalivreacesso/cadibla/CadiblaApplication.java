cbeceee9572af6f34703e6734df025b2d90eff34
IvanPix <ivan@qwsoftware.com.br>
2025-03-25 18:00:33 -0300

    Inicio do sistema de cadastro de membros

---
+++ src/main/java/br/com/batistalivreacesso/cadibla/CadiblaApplication.java
@@ -0,0 +1,13 @@
+package br.com.batistalivreacesso.cadibla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadiblaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadiblaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/batistalivreacesso/cadibla/CadiblaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.batistalivreacesso.cadibla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadiblaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
